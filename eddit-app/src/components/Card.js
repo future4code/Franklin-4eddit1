66e9531c030434fcf68ddd00f625068da4819dbb
Fernanda da Silva <nanda.drew01@gmail.com>
2022-07-22 14:55:16 -0300

    adicionando comentar

--- eddit-app/src/components/Card.js
+++ eddit-app/src/components/Card.js
@@ -1,52 +1,78 @@
-import { Box, Flex, Icon, Text } from '@chakra-ui/react';
-import { ArrowUpIcon, ArrowDownIcon, ChatIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Flex,
+  Text,
+  Fade,
+  Textarea,
+  Button,
+  Divider,
+} from '@chakra-ui/react';
+import { ChatIcon } from '@chakra-ui/icons';
 import { TbArrowBigTop, TbArrowBigDown } from 'react-icons/tb';
+import { useDisclosure } from '@chakra-ui/react';
+export const Card = ({ Texto, Autor }) => {
+  const { isOpen, onToggle } = useDisclosure();
 
-export const Card = () => {
   return (
-    <Box
-      bg={'#FBFBFB'}
-      border={'1px solid #E0E0E0'}
-      w={'100%'}
-      borderRadius={'12px'}
-      m={'15px'}
-    >
-      <Flex
-        flexDirection={'column'}
-        align={'flex-start'}
-        p={'10px 35px 10px 20px'}
+    <>
+      <Box
+        bg={'#FBFBFB'}
+        border={'1px solid #E0E0E0'}
+        w={'100%'}
+        borderRadius={'12px'}
+        m={'8px 15px 8px 15px'}
       >
-        <Text color={'#6F6F6F'}>Enviado por</Text>
-        <Text padding={'10px 0px 10px 0px'}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,
-        </Text>
-        <Flex color={'#6F6F6F'} marginBottom={'3px'}>
-          <Flex
-            alignItems={'center'}
-            border={'1px solid #e6e6e6'}
-            borderRadius={'10px'}
-            mr={'15px'}
-          >
-            <div>
-              <TbArrowBigTop style={{ margin: '0 10px', fontSize: '20px' }} />
-            </div>
-            <Text>1.2K</Text>
-            <TbArrowBigDown style={{ margin: '0 10px', fontSize: '20px' }} />
-          </Flex>
+        <Flex
+          flexDirection={'column'}
+          align={'flex-start'}
+          p={'10px 35px 10px 20px'}
+        >
+          <Text color={'#6F6F6F'}>Enviado por: {Autor}</Text>
+          <Text padding={'10px 0px 10px 0px'} wordBreak={'break-word'}>
+            {Texto}
+          </Text>
+          <Flex color={'#6F6F6F'} marginBottom={'3px'}>
+            <Flex
+              alignItems={'center'}
+              border={'1px solid #e6e6e6'}
+              borderRadius={'10px'}
+              mr={'15px'}
+            >
+              <div>
+                <TbArrowBigTop style={{ margin: '0 10px', fontSize: '20px' }} />
+              </div>
+              <Text>1.2K</Text>
+              <TbArrowBigDown style={{ margin: '0 10px', fontSize: '20px' }} />
+            </Flex>
 
-          <Flex
-            alignItems={'center'}
-            border={'1px solid #e6e6e6'}
-            borderRadius={'10px'}
-            mr={'15px'}
-          >
-            <ChatIcon m={'5px'} />
-            <Text mr={'5px'}>150</Text>
+            <Flex
+              alignItems={'center'}
+              border={'1px solid #e6e6e6'}
+              borderRadius={'10px'}
+              mr={'15px'}
+            >
+              <ChatIcon m={'5px'} cursor={'pointer'} onClick={onToggle} />
+              <Text mr={'5px'}>150</Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Box>
+      {isOpen === true ? (
+        <Box>
+          <Fade in={isOpen}>
+            <Textarea
+              placeholder="Adicionar comentário"
+              variant={'filled'}
+              marginTop={'8px'}
+              height={'100px'}
+              width={'300px'}
+              style={{ resize: 'none' }}
+            />
+            <Button variant={'solid'}>Responder</Button>
+            <Divider mb={'16px'} />
+          </Fade>
+        </Box>
+      ) : null}
+    </>
   );
 };
--- eddit-app/src/pages/HomePage/HomePage.jsx
+++ eddit-app/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Flex, Input, Textarea } from '@chakra-ui/react';
+import { Button, Divider, Flex, Textarea } from '@chakra-ui/react';
 import React from 'react';
 import { Card } from '../../components/Card';
 import Header from '../../components/Header';
@@ -13,14 +13,22 @@ export default function LoginPage() {
         p={'20px 35px 10px 35px;'}
       >
         <Textarea
-          placeholder="Here is a sample placeholder"
+          placeholder="Escreva seu post..."
           variant={'filled'}
           height={'131px'}
           style={{ resize: 'none' }}
         />
         <Button variant={'solid'}>Postar</Button>
-        <Divider />
-        <Card />
+        <Divider mb={'16px'} />
+        <Card Autor={'José'} Texto={'Blablablablablablablabla'} />
+        <Card
+          Autor={'Maria'}
+          Texto={
+            'BlablablablablablablablaBlablablablablablablablaBlablablablablablablablaBlablablablablablablablaBlablablablablablablablaBlablablablablabla'
+          }
+        />
+        <Card Autor={'João'} Texto={'Blablablablablablablabla'} />
+        <Card Autor={'Alberto'} Texto={'Blablablablablablablabla'} />
       </Flex>
     </>
   );
