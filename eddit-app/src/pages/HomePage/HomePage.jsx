43d3e663448b5dbdc9b1bbb1a0e632af42e3a926
Fernanda da Silva <nanda.drew01@gmail.com>
2022-07-28 18:28:01 -0300

    merge

--- eddit-app/src/pages/HomePage/HomePage.jsx
+++ eddit-app/src/pages/HomePage/HomePage.jsx
@@ -4,36 +4,68 @@ import { useNavigate } from 'react-router-dom';
 import { Card } from '../../components/Card';
 import Header from '../../components/Header';
 //import { InputStyled } from "./styled";
-import useRequestData from '../../hooks/useRequestData';
+import useRequestData from '../../hooks/UseRequestData';
 import { goToLogin } from '../../Routes/cordinator';
+import useForm from '../../hooks/useForm';
+import { BASE_URL } from '../../constants/urls';
+import axios from 'axios';
 
 export default function HomePage() {
+  const [form, onChange, clear] = useForm({
+    username: '',
+    email: '',
+    password: '',
+  });
   const [post, setPosts, isLoading] = useRequestData();
-  const token = localStorage.getItem('token')
-  const navigate = useNavigate()
-  const validation = () => {
-      if(!token){
-          goToLogin(navigate)
-      }
-  }
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
 
-  useEffect(validation, [token, navigate])
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    handlePost(form, clear, navigate);
+  };
+  const handlePost = (body, clear, navigate) => {
+    axios
+      .post(`${BASE_URL}/users/signup`, body, {
+        headers: {
+          ContentType: localStorage.getItem('application/json'),
+        },
+      })
+      .then((response) => {
+        localStorage.setItem('token', response.data.token);
+        clear();
+      });
+  };
+
+  const validation = () => {
+    if (!token) {
+      goToLogin(navigate);
+    }
+  };
+
+  useEffect(validation, [token, navigate]);
   console.log(post);
   return (
     <>
-      <Header typeButton={'logout'}/>
+      <Header typeButton={'logout'} />
       <Flex
         flexDirection={'column'}
         align={'center'}
         p={'20px 35px 10px 35px;'}
       >
-        <Textarea
-          placeholder="Escreva seu post..."
-          variant={'filled'}
-          height={'131px'}
-          style={{ resize: 'none' }}
-        />
-        <Button variant={'solid'}>Postar</Button>
+        <form onSubmit={onSubmitForm}>
+          <Textarea
+            placeholder="Escreva seu post..."
+            variant={'filled'}
+            height={'131px'}
+            style={{ resize: 'none' }}
+          />
+          <Button variant={'solid'} type={'submit'}>
+            Postar
+          </Button>
+        </form>
+
         <Divider mb={'16px'} />
 
         {isLoading && <p>Carregando</p>}
@@ -41,9 +73,9 @@ export default function HomePage() {
           post &&
           post.length >= 0 &&
           post.map((post) => {
-              console.log("#######################");
-              console.log(post)
-              console.log("#######################");
+            console.log('#######################');
+            console.log(post);
+            console.log('#######################');
             return (
               <Card
                 Autor={post.username}
