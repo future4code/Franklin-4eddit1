b75b9fc043cb49189d85d1ba9d17fc90a67528bb
Fernanda da Silva <nanda.drew01@gmail.com>
2022-07-31 17:07:20 -0300

    corrigindo imports

--- eddit-app/src/Routes/Router.js
+++ eddit-app/src/Routes/Router.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage/HomePage";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import SignUpPage from "../pages/SignUpPage/SignUpPage";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import SignUpPage from '../pages/SignupPage/SignupPage';
 
 export default function Router() {
   return (
